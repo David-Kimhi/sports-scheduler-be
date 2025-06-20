eb0ecec9fee5218c5d68e0442f8e5a207d1083c1
david <radakdk@gmail.com>
2025-06-20 14:30:54 +0300

    updated fetch_with_flags

--- scraper/src/fetch_with_flags.ts
+++ scraper/src/fetch_with_flags.ts
@@ -1,1 +1,15 @@
-// index.ts
+import { fetchAndStoreCountries } from './sports/football/fetchCountries.js'
+import {  fetchAndStoreLeauges } from './sports/football/fetchLeagues.js'
+import { fetchAndStoreFixtures } from './sports/football/fetchGames.js';
+import { runOnce, resetFlags } from './sports/football/flags.js';
+
+resetFlags();
+
+// Countries
+await runOnce("fetchCountries", fetchAndStoreCountries);
+
+// Leagues
+await runOnce("fetchLeagues", fetchAndStoreLeauges);
+
+// Fixtures
+await runOnce("fetchFixtures", fetchAndStoreFixtures);
