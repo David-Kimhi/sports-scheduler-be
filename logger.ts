6520ca16b7f0534c0717a6830fc5021ade2d5d0e
david <radakdk@gmail.com>
2025-06-20 16:04:30 +0300

    Add logs if doesn't exist

--- logger.ts
+++ logger.ts
@@ -1,33 +1,38 @@
 import winston from 'winston';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 
 // Recreate __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-
 /**
  * Creates a winston logger with the given name.
  */
 export function createLogger(loggerName: string) {
-
-  // Define log directory and log file path
+    // Define log directory and log file path
     const logDir = path.resolve(__dirname, './logs');
+
+    // Ensure the logs directory exists
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir, { recursive: true });
+    }
+
     const logFilePath = path.join(logDir, `${loggerName}.log`);
+
     const logger = winston.createLogger({
         level: 'info',
         format: winston.format.combine(
-        winston.format.label({ label: loggerName }),
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.printf(({ timestamp, level, message, label }) => {
-            return `${timestamp} [${label}] ${level}: ${message}`;
-        })
+            winston.format.label({ label: loggerName }),
+            winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            winston.format.printf(({ timestamp, level, message, label }) => {
+                return `${timestamp} [${label}] ${level}: ${message}`;
+            })
         ),
         transports: [
-        new winston.transports.File({ filename: logFilePath }),
-        new winston.transports.Console()
+            new winston.transports.File({ filename: logFilePath }),
+            new winston.transports.Console()
         ]
     });
 
