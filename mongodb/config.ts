aa1e4ad6e0e9844697ba9e63f85c296be6e0a951
david <radakdk@gmail.com>
2025-06-20 15:33:39 +0300

    fixattempt: put the default db name

--- mongodb/config.ts
+++ mongodb/config.ts
@@ -8,6 +8,8 @@ export const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD!);
 export const CLUSTER = 'cluster0.raxoh.mongodb.net';
 export const OPTIONS = '?retryWrites=true&w=majority&appName=Cluster0';
 
-export const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER}/${OPTIONS}`;
+export const DEFAULT_DB_NAME = 'football';
+
+export const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER}/${DEFAULT_DB_NAME}${OPTIONS}`;
 
 export const MODULE = 'MongoDB'
