7ef3f9590a002ef9d9859e9397b2216ab078b304
david <radakdk@gmail.com>
2025-06-22 00:34:53 +0300

    fix paths

--- docker-run-scraper.sh
+++ docker-run-scraper.sh
@@ -4,6 +4,6 @@ cd sports-scheduler-be/
 
 timeout 3h docker run --rm \
   --env-file .env \
-  -v sports-scheduler-be/logs:sports-scheduler-be/dist/logs \
-  -v sports-scheduler-be/data/flags.json:sports-scheduler-be/dist/scraper/src/sports/football/flags/flags.json \
+  -v /root/sports-scheduler-be/logs:/root/sports-scheduler-be/dist/logs \
+  -v /root/sports-scheduler-be/data/flags.json:/root/sports-scheduler-be/dist/scraper/src/sports/football/flags/flags.json \
   sport-scheduler
