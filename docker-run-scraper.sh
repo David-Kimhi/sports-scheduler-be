cdac33412f763ed00f21a20f0615b5ffe167d67b
david <radakdk@gmail.com>
2025-06-21 14:41:04 +0300

    added docker run file

---
+++ docker-run-scraper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /sports-scheduler-be
+
+timeout 3h docker run --rm \
+  --env-file .env \
+  -v /sports-scheduler-be/logs:/sports-scheduler-be/dist/logs \
+  -v /sports-scheduler-be/data/flags.json:/sports-scheduler-be/dist/scraper/src/sports/football/flags/flags.json \
+  sport-scheduler
