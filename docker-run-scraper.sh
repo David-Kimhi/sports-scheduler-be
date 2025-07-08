815bec59422d84692385412f0438ba4137bebb39
david server <radakdk@gmail.com>
2025-07-08 13:49:23 +0300

    changed dockerfile.scraper command

--- docker-run-scraper.sh
+++ docker-run-scraper.sh
@@ -4,6 +4,6 @@ cd sports-scheduler-be/
 
 timeout 3h docker run --rm \
   --env-file .env \
-  -v /root/sports-scheduler-be/logs:/root/sports-scheduler-be/dist/logs \
-  -v /root/sports-scheduler-be/data/flags.json:/root/sports-scheduler-be/dist/data/flags.json \
-  sports-scheduler
+  -v /root/sports-scheduler-be/logs:/app/dist/logs \
+  -v /root/sports-scheduler-be/data/flags.json:/app/dist/data/flags.json \
+  scheduler-scraper
