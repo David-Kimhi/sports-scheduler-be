0b1ad1912bea6a3650766853af241fdb5aedf5ec
david <radakdk@gmail.com>
2025-07-19 12:48:23 +0300

    allow localhost origin

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import footballApi from './src/routs/api.js';
 import { LOCAL_PORT } from './src/config/index.js';
 import { Game, Country, League } from './src/models/index.js';
 import { SPORT, GAMES_COLL_NAME, COUNTRIES_COLL_NAME, LEAGUES_COLL_NAME } from './src/config/index.js';
+import cors from 'cors';
+
 
 const app = express();
 
@@ -10,6 +12,9 @@ await Game.init(SPORT, GAMES_COLL_NAME, 'api');
 await Country.init(SPORT, COUNTRIES_COLL_NAME, 'api');
 await League.init(SPORT, LEAGUES_COLL_NAME, 'api');
 
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use(express.json());
 app.use('/api/football', footballApi);
 
