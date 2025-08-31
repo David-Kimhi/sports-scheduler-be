fb7a02f5cfa0c66167bd1234fd4c809377a0bd8e
david <radakdk@gmail.com>
2025-08-31 23:38:22 +0300

    update docker compose

--- src/tests/scraper_tests.ts
+++ src/tests/scraper_tests.ts
@@ -1,8 +1,15 @@
 import { populateLeagueTeams } from "../scripts/populateLeagues.js";
 import { closeMongoDb, getMongoDb } from "../services/index.js";
-import { SPORT, SCRAPER_MODULE } from "../config/index.js";
+import { SPORT, SCRAPER_MODULE, IS_PRO_PLAN } from "../config/index.js";
 import { cleanTeams } from "../scrapers/football/fetchTeams.js";
 
 await cleanTeams();
 await closeMongoDb(SPORT, SCRAPER_MODULE);
+
+import { FlagsManager } from "../services/index.js";
+
+const flasgsManager = new FlagsManager();
+
+
+console.log(IS_PRO_PLAN)
 
