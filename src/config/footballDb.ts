92e0094cdde2a17295137bcd06db9768066b22c8
david server <radakdk@gmail.com>
2025-10-03 14:19:59 +0300

    change cluster

--- src/config/footballDb.ts
+++ src/config/footballDb.ts
@@ -1,7 +1,7 @@
 export const USERNAME = encodeURIComponent(process.env.DB_USERNAME!);
 export const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD!);
-export const CLUSTER = 'cluster0.raxoh.mongodb.net';
-export const OPTIONS = '?retryWrites=true&w=majority&appName=Cluster0';
+export const CLUSTER = 'cluster1.jjaidiu.mongodb.net';
+export const OPTIONS = '?retryWrites=true&w=majority&appName=Cluster1';
 
 export const DEFAULT_DB_NAME = 'football';
 
